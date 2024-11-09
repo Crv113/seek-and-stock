004e165b3add887638e36fe77b44b33179b35bb2
Adrien Corvisier <adrien.corvisier.17@gmail.com>
2024-11-09 20:04:15 +0100

    add apikey middleware

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\EventController;
+use App\Http\Middleware\VerifyApiKey;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +9,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('events', EventController::class);
-Route::get('events/{id}/results', [EventController::class, 'getEventResults']);
+Route::middleware([VerifyApiKey::class])->group(function () {
+    Route::apiResource('events', EventController::class);
+    Route::get('events/{id}/results', [EventController::class, 'getEventResults']);
+});
+
