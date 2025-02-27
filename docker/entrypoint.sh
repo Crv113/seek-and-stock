1b8e78642b6ec6d7bad441780e6f8940747c0153
Adrien Corvisier <adrien.corvisier.17@gmail.com>
2025-02-27 18:07:53 +0100

    wip entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-echo "Fixing permissions..."
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
-echo "Starting PHP-FPM..."
-# Exécuter le processus principal (PHP-FPM)
+echo "Checking storage permissions..."
+CURRENT_UID=$(stat -c "%u" /var/www/html/storage)
+CURRENT_GID=$(stat -c "%g" /var/www/html/storage)
+
+if [ "$CURRENT_UID" != "33" ] || [ "$CURRENT_GID" != "33" ]; then
+    echo "Fixing permissions for storage..."
+    chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+    chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+else
+    echo "Permissions are already correct."
+fi
+
+# Démarrer PHP-FPM après avoir appliqué les permissions
 exec "$@"
