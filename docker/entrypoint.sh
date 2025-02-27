40e9f522924d6b2b075580f85f6a969a25cedd3e
Adrien Corvisier <adrien.corvisier.17@gmail.com>
2025-02-27 17:57:18 +0100

    wip entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,6 @@
 echo "Fixing permissions..."
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
-
+echo "Starting PHP-FPM..."
 # Exécuter le processus principal (PHP-FPM)
 exec "$@"
