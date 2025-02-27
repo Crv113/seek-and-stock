bdc58ff13302ee45d64e5e856d69481e0f17b9cd
Adrien Corvisier <adrien.corvisier.17@gmail.com>
2025-02-27 17:36:13 +0100

    wip dockerfile

---
+++ docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Fixing permissions..."
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+# Exécuter le processus principal (PHP-FPM)
+exec "$@"
