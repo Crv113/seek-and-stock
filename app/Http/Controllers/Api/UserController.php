f4ae31ca57bafe60f076389e9357b68fff1d3648
Adrien Corvisier <adrien.corvisier.17@gmail.com>
2026-05-14 12:39:22 +0200

    ajout stats dans user index

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -10,15 +10,73 @@ use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     function index()
     {
-        return User::select('id', 'name', 'discord_id', 'discord_global_name', 'discord_avatar')
+        $users = User::select('id', 'name', 'discord_id', 'discord_global_name', 'discord_avatar')
             ->orderBy('name')
             ->get();
+
+        $userIds = $users->pluck('id');
+
+        // Participation count: distinct events per user
+        $participations = DB::table('lap_times')
+            ->join('users as u', 'u.guid', '=', 'lap_times.player_guid')
+            ->whereIn('u.id', $userIds)
+            ->select('u.id', DB::raw('COUNT(DISTINCT lap_times.event_id) as cnt'))
+            ->groupBy('u.id')
+            ->pluck('cnt', 'u.id');
+
+        // Victory count: finished events won per user (tie-break by MIN lap id)
+        $bestTimesPerEvent = DB::table('lap_times')
+            ->join('events', 'events.id', '=', 'lap_times.event_id')
+            ->join('users as u', 'u.guid', '=', 'lap_times.player_guid')
+            ->where('events.ending_date', '<=', now())
+            ->select('lap_times.event_id', DB::raw('MIN(lap_times.lap_time) as best_time'))
+            ->groupBy('lap_times.event_id');
+
+        $winners = DB::table('lap_times as lt')
+            ->joinSub($bestTimesPerEvent, 'best', function ($join) {
+                $join->on('lt.event_id', '=', 'best.event_id')
+                     ->on('lt.lap_time', '=', 'best.best_time');
+            })
+            ->join('users as u', 'u.guid', '=', 'lt.player_guid')
+            ->select('lt.event_id', DB::raw('MIN(lt.id) as winning_lap_id'))
+            ->groupBy('lt.event_id');
+
+        $victories = DB::table('lap_times as lt')
+            ->joinSub($winners, 'w', 'lt.id', '=', 'w.winning_lap_id')
+            ->join('users as u', 'u.guid', '=', 'lt.player_guid')
+            ->whereIn('u.id', $userIds)
+            ->select('u.id', DB::raw('COUNT(*) as cnt'))
+            ->groupBy('u.id')
+            ->pluck('cnt', 'u.id');
+
+        // Favorite bike: most used bike across all lap_times per user
+        $favoriteBikes = DB::table('lap_times')
+            ->join('users as u', 'u.guid', '=', 'lap_times.player_guid')
+            ->join('bikes', 'bikes.id', '=', 'lap_times.bike_id')
+            ->whereIn('u.id', $userIds)
+            ->select('u.id as user_id', 'bikes.name as bike_name', DB::raw('COUNT(*) as cnt'))
+            ->groupBy('u.id', 'bikes.name')
+            ->get()
+            ->groupBy('user_id')
+            ->map(fn($bikes) => $bikes->sortByDesc('cnt')->first()->bike_name);
+
+        return $users->map(fn($user) => [
+            'id'                  => $user->id,
+            'name'                => $user->name,
+            'discord_id'          => $user->discord_id,
+            'discord_global_name' => $user->discord_global_name,
+            'discord_avatar'      => $user->discord_avatar,
+            'participation_count' => $participations[$user->id] ?? 0,
+            'victory_count'       => $victories[$user->id] ?? 0,
+            'favorite_bike'       => $favoriteBikes[$user->id] ?? null,
+        ]);
     }
 
     function show(User $user, GetUserBestLapTimes $get_user_best_lap_times, GetUserVictories $get_user_victories): UserResource
